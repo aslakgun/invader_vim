34f35579db2dd343ca2b17a286c9f83dbbeab6f2
Logan <loganlan2001@gmail.com>
2025-11-10 12:58:03 -0800

    added aliens

--- settings.py
+++ settings.py
@@ -2,6 +2,11 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
         """Initialize the game's settings."""
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
